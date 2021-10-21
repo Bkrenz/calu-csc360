53cae8f54200dcf0cdd63dac74b0b8229c5dabd5
Robert Krency <bobkrency@gmail.com>
2021-10-21 17:45:03 -0400

    Basic project setup

---
+++ Assignments/Minimum Dominating Set/src/main.cpp
@@ -0,0 +1,17 @@
+/** *
+ * 
+ * Author: Frank Bedekovich
+ * Author: Robert Krency
+ * Author: Regina Thase 
+ * 
+ */
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+
+}
